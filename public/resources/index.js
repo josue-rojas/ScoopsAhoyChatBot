29bb55facc1d4eec9b5789f3f2bbcbbfdd0fa576
josue rojas <josuerojas.rojas@gmail.com>
2019-08-01 20:44:54 -0400

    styled chatbox

--- public/resources/index.js
+++ public/resources/index.js
@@ -1,129 +1,131 @@
-$(function() {
-  // Get handle to the chat div
-  var $chatWindow = $('#messages');
+let $chatWindow = document.getElementById('messages');
+let chatClient;
+let thisChannel;
+let username;
+let channelName;
 
-  // Our interface to the Chat service
-  var chatClient;
+function print(infoMessage, asHtml) {
+  let $msg = document.createElement('div');
+  $msg.classList.add('info');
+  if(asHtml) $msg.innerHTML = infoMessage;
+  else $msg.innerText = infoMessage;
+  $chatWindow.appendChild($msg);
+}
 
-  // A handle to the "general" chat channel - the one and only channel we
-  // will have in this sample app
-  var generalChannel;
+function printMessage(fromUser, message) {
+  // let $user = document.createElement('span');
+  // $user.classList.add('username');
+  // $user.innerText = `${fromUser} : `;
+  let $message = document.createElement('span');
+  $message.classList.add('message');
+  $message.innerText = message;
+  let $container = document.createElement('div');
+  if(fromUser === username) $container.classList.add( 'me');
+  $container.classList.add('message-container');
+  // $container.appendChild($user)
+  $container.appendChild($message);
+  $chatWindow.appendChild($container);
+  $chatWindow.scrollTo(0, $chatWindow.scrollHeight);
+}
 
-  // The server will assign the client a random username - store that value
-  // here
-  var username;
-
-  // Helper function to print info messages to the chat window
-  function print(infoMessage, asHtml) {
-    let $msg = document.createElement('div');
-    $msg.setAttribute('class', 'info');
-    if(asHtml) $msg.innerHTML = infoMessage;
-    else $msg.innerText = infoMessage;
-    console.log($msg);
-    // neet to replace for vanilla
-    $chatWindow.append($msg);
-  }
-
-  // Helper function to print chat message to the chat window
-  function printMessage(fromUser, message) {
-    var $user = $('<span class="username">').text(fromUser + ':');
-    if (fromUser === username) {
-      $user.addClass('me');
-    }
-    var $message = $('<span class="message">').text(message);
-    var $container = $('<div class="message-container">');
-    $container.append($user).append($message);
-    $chatWindow.append($container);
-    $chatWindow.scrollTop($chatWindow[0].scrollHeight);
-  }
-
-  // function printMessage(fromUser, message) {
-  //
-  // }
-
-  // Alert the user they have been assigned a random username
-  print('Logging in...');
-
-  // Get an access token for the current user, passing a username (identity)
-  // and a device ID - for browser-based apps, we'll always just use the
-  // value "browser"
-  $.getJSON('/token', {
-    device: 'browser'
-  }, function(data) {
-
-
-    // Initialize the Chat client
-    Twilio.Chat.Client.create(data.token).then(client => {
+function startChat() {
+  fetch('/token', { device: 'browser' })
+  .then( res => res.json())
+  .then( data => {
+    Twilio.Chat.Client.create(data.token)
+    .then(client => {
       console.log('Created chat client');
       chatClient = client;
-      console.log('client',client)
-      chatClient.getSubscribedChannels().then(createOrJoinGeneralChannel);
+      console.log('client', client);
+      chatClient.getSubscribedChannels()
+      .then(createOrJoinNewChannel)
+      username = data.identity;
+      channelName = `${username}-${(new Date()).getTime()}`;
+    })
+  })
+}
 
-    // Alert the user they have been assigned a random username
-    username = data.identity;
-    print('You have been assigned a random username of: '
-    + '<span class="me">' + username + '</span>', true);
-
-    }).catch(error => {
-      console.error(error);
-      print('There was an error creating the chat client:<br/>' + error, true);
-      print('Please check your .env file.', false);
-    });
-  });
-
-  function createOrJoinGeneralChannel() {
-    // Get the general chat channel, which is where all the messages are
-    // sent in this simple application
-    print('Attempting to join "general" chat channel...');
-    chatClient.getChannelByUniqueName('general')
-    .then(function(channel) {
-      generalChannel = channel;
-      console.log('Found general channel:');
-      console.log(generalChannel);
+// might be bad to create a bunch of channels everytime it refreshes (should probably save the data and restore it (save data like username and channel name))
+function createOrJoinNewChannel() {
+  console.log('creating new channel');
+  chatClient.getChannelByUniqueName(channelName)
+  .then((channel) => {
+    thisChannel = channel;
+    console.log('Found user\'s unique channel:');
+    console.log(generalChannel);
+    setupChannel();
+  })
+  .catch(() => {
+    // If it doesn't exist, let's create it
+    console.log('Creating general channel');
+    chatClient.createChannel({
+      uniqueName: channelName,
+      friendlyName: `${username} - unique channel for bot`
+    })
+    .then((channel) => {
+      console.log('Created new unique channel');
+      thisChannel = channel;
       setupChannel();
-    }).catch(function() {
-      // If it doesn't exist, let's create it
-      console.log('Creating general channel');
-      chatClient.createChannel({
-        uniqueName: 'general',
-        friendlyName: 'General Chat Channel'
-      }).then(function(channel) {
-        console.log('Created general channel:');
-        console.log(channel);
-        generalChannel = channel;
-        setupChannel();
-      }).catch(function(channel) {
-        console.log('Channel could not be created:');
-        console.log(channel);
-      });
-    });
-  }
-
-  // Set up channel after it has been found
-  function setupChannel() {
-    // Join the general channel
-    generalChannel.join().then(function(channel) {
-      print('Joined channel as '
-      + '<span class="me">' + username + '</span>.', true);
-    });
-
-    // Listen for new messages sent to the channel
-    generalChannel.on('messageAdded', function(message) {
-      printMessage(message.author, message.body);
-    });
-  }
-
-  // Send a new message to the general channel
-  var $input = $('#chat-input');
-  $input.on('keydown', function(e) {
-
-    if (e.keyCode == 13) {
-      if (generalChannel === undefined) {
-        print('The Chat Service is not configured. Please check your .env file.', false);
-        return;
-      }
-      generalChannel.sendMessage($input.val())
-      $input.val('');
-    }
+    })
+    .catch((channel) => {
+      console.log('Channel could not be created');
+      console.log(channel);
+    })
   });
-});
+}
+
+// Set up channel after it has been found
+function setupChannel() {
+  thisChannel.join().then((channel) => {
+    print('Joined channel as '
+    + '<span class="my-name">' + username + '</span>.', true);
+  });
+  // Listen for new messages sent to the channel
+  thisChannel.on('messageAdded', function(message) {
+    printMessage(message.author, message.body);
+  });
+}
+
+let input = document.getElementById('chat-input')
+input.addEventListener('keydown', (event) =>{
+  if(event.keyCode == 13) {
+    if(thisChannel == undefined){
+      print('The Chat Service is not configured. Please check your .env file.', false);
+      return;
+    }
+    thisChannel.sendMessage(input.value);
+    input.value = '';
+  }
+})
+
+// hide chat
+let $wholeChat = document.querySelector('section.chat-box-wrapper');
+let isChatActive = false;
+let isInit = true;
+$wholeChat.addEventListener('click', (event) => {
+  event.stopPropagation();
+  if(event.target == $wholeChat && isChatActive) {
+    $wholeChat.classList.remove('active');
+    isChatActive = false;
+    // totally remove the element so everything else can be clickable
+    setTimeout(() => {
+      $wholeChat.style.display = 'none';
+    }, 360);
+  }
+})
+
+let $chatButton = document.querySelector('div.chat-button');
+$chatButton.addEventListener('click', (event) => {
+  if(!isChatActive) {
+    $wholeChat.style.display = 'flex';
+    // delay a bit so it has a transition and not so instant
+    setTimeout(() => {
+      $wholeChat.classList.add('active');
+      if(isInit) {
+        startChat();
+        isInit = false;
+      }
+    }, 1)
+    isChatActive = true;
+  }
+})
